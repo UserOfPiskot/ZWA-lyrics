a8e4f07a9f2cc426474d7861a723fbbd7693e323
UserOfPiskot <143938429+UserOfPiskot@users.noreply.github.com>
2025-06-17 14:15:52 +0200

    Added db login by .env

--- config/config.php
+++ config/config.php
@@ -1,20 +1,11 @@
 <?php
-/*foreach ($lines as $line){
-
-}
-
-$dbHost = $_ENV['DB_HOST'];
-$dbUser = $_ENV['DB_USER'];
-$dbPass = $_ENV['DB_PASS'];
-$dbName = $_ENV['DB_NAME'];
+$dbHost = $_ENV['DB_HOSTNAME'];
+$dbUser = $_ENV['DB_USERNAME'];
+$dbPass = $_ENV['DB_PASSWORD'];
+$dbName = $_ENV['DB_DATABASE'];
 
 // Now you can use these variables to connect to your database
 $conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-mysqli_set_charset($conn, "UTF8");*/
-$conn = mysqli_connect("localhost", "root", "", "web_db");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
--- includes/session.php
+++ includes/session.php
@@ -3,5 +3,6 @@ if(session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
+require_once __DIR__ . "/../includes/getenv.php";
 require_once __DIR__ . "/../config/config.php";
 require_once __DIR__ . "/../includes/helpers.php";
